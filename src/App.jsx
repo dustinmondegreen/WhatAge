0da004f86b420fc68aa688b75339d358dd9736a2
Franscelino <francelinomelvyn@gmail.com>
2025-03-20 05:18:37 +0700

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <Router>
-      {/* <Navbar /> */}
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
