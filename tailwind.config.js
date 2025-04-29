bb146524e17cca5038eb748ba558af4a38adb899
dustinmondegreen <antowilliam1029@gmail.com>
2025-04-29 23:19:30 +0700

    Fix postcss Tailwind CSS error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: ["./index.html", "./src/**/*.{js,ts}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+module.exports = {
+  content: [
+    './src/**/*.{html,js}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
