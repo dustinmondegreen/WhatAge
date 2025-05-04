57d58555d9ecc1c6c0d42f4bacf02ac0eb1d8aae
Akbar <akbarzaidanr2@gmail.com>
2025-05-04 16:49:59 +0700

    update

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-const startBtn = document.getElementById('start-btn');
-const zeroPage = document.getElementById('zero-page');
-const testPage = document.getElementById('test-page');
+document.addEventListener("DOMContentLoaded", function() {
+  const sections = document.querySelectorAll("section");
+  sections.forEach(section => {
+    section.style.display = "none";
+  });
 
-startBtn.addEventListener('click', function() {
-  zeroPage.style.display = 'none';
-  testPage.style.display = 'block';
+  document.getElementById("test-page-1").style.display = "block";
+
+  document.getElementById("test-page-1-btn-next").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("test-page-1").style.display = "none";
+    document.getElementById("test-page-2").style.display = "block";
+  });
+
+  document.getElementById("test-page-2-btn-next").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("test-page-2").style.display = "none";
+    document.getElementById("test-page-3").style.display = "block";
+  });
+
+  document.getElementById("test-page-2-btn-back").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("test-page-2").style.display = "none";
+    document.getElementById("test-page-1").style.display = "block";
+  });
+
+  document.getElementById("test-page-3-btn-back").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("test-page-3").style.display = "none";
+    document.getElementById("test-page-2").style.display = "block";
+  });
 });
