495c45ad499cd5a373f6064e1d39ce22b1988e20
dustinmondegreen <antowilliam1029@gmail.com>
2025-05-13 22:42:47 +0700

    Add SQLAlchemy and specify schema, use zero.html as root html

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    userID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    userName = db.Column(db.String(100), nullable=False)
+    userEmail = db.Column(db.String(120), unique=True, nullable=False)
+    UserPassword = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"<Current user is {self.UserName}>"
+
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("zero.html")
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
